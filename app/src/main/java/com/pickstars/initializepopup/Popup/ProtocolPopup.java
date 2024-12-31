13c7f62bb5fde90863ef22d35fa4cbf5d0426cb2
PickStars308 <starstoolbox@outlook.com>
2024-12-31 18:06:31 +0800

    feat(initializepopup): Add clickable links to the user agreement and privacy policy
    - Add ClickableTextHelper class to implement clickable text
    - Add links to the user agreement and privacy policy in the agreement pop-up window- Add Toast prompt when clicking the link
    - Add a checkbox to confirm that the user has read and agreed to the agreement
    - Optimize the display format of the agreement content

--- app/src/main/java/com/pickstars/initializepopup/Popup/ProtocolPopup.java
+++ app/src/main/java/com/pickstars/initializepopup/Popup/ProtocolPopup.java
@@ -1,18 +1,25 @@
 package com.pickstars.initializepopup.Popup;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import com.lxj.xpopup.animator.PopupAnimator;
 import com.lxj.xpopup.core.BottomPopupView;
-import com.pickstars.initializepopup.MainActivity;
 import com.pickstars.initializepopup.R;
 import com.pickstars.initializepopup.SharedPreferencesHelper;
+import com.pickstars.initializepopup.Utils.ClickableTextHelper;
 import com.pickstars.initializepopup.WelcomeActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProtocolPopup extends BottomPopupView {
     //注意：自定义弹窗本质是一个自定义View，但是只需重写一个参数的构造，其他的不要重写，所有的自定义弹窗都是这样。
@@ -25,10 +32,39 @@ public class ProtocolPopup extends BottomPopupView {
         return R.layout.protocol_popup;
     }
     // 执行初始化操作，比如：findView，设置点击，或者任何你弹窗内的业务逻辑
+    @SuppressLint("InlinedApi")
     @Override
     protected void onCreate() {
         super.onCreate();
+        CheckBox CheckBox = findViewById(R.id.CheckBox);
+
+        LinearLayout ReadTheAgreementLayout = findViewById(R.id.ReadTheAgreementLayout);
+
+        ReadTheAgreementLayout.setOnClickListener(v->{
+            CheckBox.setChecked(!CheckBox.isChecked());
+        });
+
+        TextView ReadTheAgreement = findViewById(R.id.ReadTheAgreement);
+
+        // 创建可点击文本列表
+        List<ClickableTextHelper.ClickableText> clickableTexts = new ArrayList<>();
+        clickableTexts.add(new ClickableTextHelper.ClickableText(getContext().getString(R.string.UserAgreement), v -> {
+            Toast.makeText(getContext(), "你点击了： 用户协议", Toast.LENGTH_SHORT).show();
+        }));
+        clickableTexts.add(new ClickableTextHelper.ClickableText(getContext().getString(R.string.PrivacyPolicy), v -> {
+            Toast.makeText(getContext(), "你点击了： 隐私政策", Toast.LENGTH_SHORT).show();
+        }));
+
+        // 设置可点击文本
+        ClickableTextHelper.setClickableText(ReadTheAgreement, getContext().getString(R.string.dialog_message), clickableTexts, ContextCompat.getColor(getContext(), android.R.color.system_on_primary_light));
+
+
         findViewById(R.id.AgreeButton).setOnClickListener(v -> {
+            if (!CheckBox.isChecked()) {
+                Toast.makeText(getContext(), "请先阅读并同意协议", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
             Toast.makeText(getContext(), " AgreeButton Test ", Toast.LENGTH_SHORT).show();
             SharedPreferencesHelper.commitBoolean("FirstRun", true);
             dismiss();
