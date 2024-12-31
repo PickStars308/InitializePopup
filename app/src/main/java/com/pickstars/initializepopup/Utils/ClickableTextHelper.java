3ba3e49607acdaf093898a4c018e3fe46e5f70e2
PickStars308 <starstoolbox@outlook.com>
2024-12-31 18:06:31 +0800

    feat(initializepopup): Add clickable links to the user agreement and privacy policy
    - Add ClickableTextHelper class to implement clickable text
    - Add links to the user agreement and privacy policy in the agreement pop-up window- Add Toast prompt when clicking the link
    - Add a checkbox to confirm that the user has read and agreed to the agreement
    - Optimize the display format of the agreement content

---
+++ app/src/main/java/com/pickstars/initializepopup/Utils/ClickableTextHelper.java
@@ -0,0 +1,76 @@
+package com.pickstars.initializepopup.Utils;
+
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import java.util.List;
+
+public class ClickableTextHelper {
+
+    public static void setClickableText(TextView textView, String fullText, List<ClickableText> clickableTexts, @ColorInt int linkColor) {
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(fullText);
+
+        // 遍历每个可点击文本项，并添加到 SpannableStringBuilder 中
+        for (ClickableText clickableText : clickableTexts) {
+            int start = fullText.indexOf(clickableText.getText());
+            if (start != -1) {
+                spannableStringBuilder.setSpan(new CustomClickableSpan(clickableText.getListener(), linkColor), start,
+                        start + clickableText.getText().length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+        }
+
+        textView.setText(spannableStringBuilder);
+        textView.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
+        textView.setHighlightColor(ContextCompat.getColor(textView.getContext(), android.R.color.transparent));
+    }
+
+    public static class ClickableText {
+        private final String text;
+        private final View.OnClickListener listener;
+
+        public ClickableText(String text, View.OnClickListener listener) {
+            this.text = text;
+            this.listener = listener;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public View.OnClickListener getListener() {
+            return listener;
+        }
+    }
+
+    // 自定义 ClickableSpan 实现，支持点击事件和高亮颜色
+    private static class CustomClickableSpan extends ClickableSpan {
+        private final View.OnClickListener mListener;
+        private final int mLinkColor;
+
+        CustomClickableSpan(View.OnClickListener listener, int linkColor) {
+            mListener = listener;
+            mLinkColor = linkColor;
+        }
+
+        @Override
+        public void onClick(@NonNull View widget) {
+            if (mListener != null) mListener.onClick(widget);
+        }
+
+        @Override
+        public void updateDrawState(@NonNull TextPaint ds) {
+            super.updateDrawState(ds);
+            ds.setColor(mLinkColor); // 设置高亮颜色
+            ds.setUnderlineText(false); // 取消下划线
+        }
+    }
+}
+
